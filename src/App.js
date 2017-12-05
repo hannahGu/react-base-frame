c46ceedf8753765a5617405bfc6c01dd617dd2f2
hannah <hannah@hannahs-MacBook-Pro.local>
2017-12-05 11:55:38 +0800

    add mine page

--- src/App.js
+++ src/App.js
@@ -1,55 +1,74 @@
 import React from "react";
-import {TabBar} from 'antd-mobile';
-import Basic from './Basic';
-const items =[{title:'外卖',icon:'https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg',key:'/main'},
-{title:'订单',icon:'https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg',key:'/order'},
-{title:'我的',icon:'https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg',key:'/mine'},];
+import { TabBar } from "antd-mobile";
+import Basic from "./Basic";
+const items = [
+  {
+    title: "外卖",
+    icon: "https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg",
+    key: "/main"
+  },
+  {
+    title: "订单",
+    icon: "https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg",
+    key: "/order"
+  },
+  {
+    title: "我的",
+    icon: "https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg",
+    key: "/mine"
+  }
+];
 export default class App extends Basic {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      selectedTab:'main'
+      selectedTab: "main"
     };
   }
   render() {
-    return <div>
-    <div style={{ height: '100%', width: '100%',position:'fixed',zIndex:999 } }>
-     <TabBar
-       unselectedTintColor="#949494"
-       tintColor="#33A3F4"
-       barTintColor="white"
-      //  hidden={this.state.hidden}
-     >
-     {items.map((item,i)=>(
-       <TabBar.Item
-         title={item.title}
-         key={item.title}
-         icon={<div style={{
-           width: '22px',
-           height: '22px',
-           background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
-         />
-         }
-         selectedIcon={<div style={{
-           width: '22px',
-           height: '22px',
-           background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
-         />
-         }
-         selected={this.state.selectedTab === item.key}
-         onPress={() => {
-           this.setState({
-             selectedTab: item.key,
-           });
-           this._forward(item.key);
-         }}
-        //  data-seed="logId"
+    return (
+      <div>
+        {this.props.children}
+        {/*
+      <div style={{ height: '100%', width: '100%',position:'fixed',zIndex:999 } }>
+       <TabBar
+         unselectedTintColor="#949494"
+         tintColor="#33A3F4"
+         barTintColor="white"
+        //  hidden={this.state.hidden}
        >
-         {this.props.children}
-       </TabBar.Item>
-     ))}
-       </TabBar>
-       </div>
-    </div>;
+       {items.map((item,i)=>(
+         <TabBar.Item
+           title={item.title}
+           key={item.title}
+           icon={<div style={{
+             width: '22px',
+             height: '22px',
+             background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+           />
+           }
+           selectedIcon={<div style={{
+             width: '22px',
+             height: '22px',
+             background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+           />
+           }
+           selected={this.state.selectedTab === item.key}
+           onPress={() => {
+             this.setState({
+               selectedTab: item.key,
+             });
+             this._forward(item.key);
+           }}
+          //  data-seed="logId"
+         >
+           {this.props.children}
+         </TabBar.Item>
+       ))}
+         </TabBar>
+         </div>
+      */}
+      </div>
+    );
   }
 }
--- src/Order.js
+++ src/Order.js
@@ -1,42 +1,44 @@
-import React from 'react';
-import Basic from './Basic';
-import {NavBar,List,Icon} from 'antd-mobile';
-import {falseOrder} from './falsedata/falsedata';
+import React from "react";
+import Basic from "./Basic";
+import { NavBar, List, Icon } from "antd-mobile";
+import { falseOrder } from "./falsedata/falsedata";
 const Item = List.Item;
 const Brief = Item.Brief;
-export default class Order extends Basic{
-  render(){
-    return(
+export default class Order extends Basic {
+  render() {
+    return (
       <div>
-      <NavBar
-    mode="light"
-    icon={<Icon type="left" />}
-    onLeftClick={(e)=>console.log('e',e)}
-  >订单</NavBar>
-  <List>
-  {falseOrder.map((item,i)=>(
-      <Item
-        key={`${item['restaurant_id']}_${i}`}
-        thumb={require(`./images/slider-pic/slider-pic2.jpg`)}
-        multipleLine
-        onClick={() => {}}
-        extra={
-          <div>
-          <p style={{marginTop:28}}>{item.order_state}</p>
-          <hr />
-          <p>{item.order_price}</p>
-          </div>
-        }
-      >
-        {item['store_name']}
-        <Brief>
-          <p>{item['order_birth_time']}</p>
-        </Brief>
-        <hr />
-        <p>{item['order_brief_info']}</p>
-      </Item>
-  ))}
-  </List>
+        <NavBar
+          mode="light"
+          icon={<Icon type="left" />}
+          onLeftClick={e => this._back}
+        >
+          订单
+        </NavBar>
+        <List>
+          {falseOrder.map((item, i) => (
+            <Item
+              key={`${item["restaurant_id"]}_${i}`}
+              thumb={require(`./images/slider-pic/slider-pic2.jpg`)}
+              multipleLine
+              onClick={() => {}}
+              extra={
+                <div>
+                  <p style={{ marginTop: 28 }}>{item.order_state}</p>
+                  <hr />
+                  <p>{item.order_price}</p>
+                </div>
+              }
+            >
+              {item["store_name"]}
+              <Brief>
+                <p>{item["order_birth_time"]}</p>
+              </Brief>
+              <hr />
+              <p>{item["order_brief_info"]}</p>
+            </Item>
+          ))}
+        </List>
       </div>
     );
   }
